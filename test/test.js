5c914f92f3717b64146ff62df19a73e70ae1d8cd
Tim Caswell <tim@creationix.com>
2010-04-02 02:02:09 -0500

    Test and update the sample codes.

--- test/test.js
+++ test/test.js
@@ -3,43 +3,43 @@ var Compose = require('composer');
 var fs = require('fs');
 var sys = require('sys');
 
-// 
-// Compose({
-//   A: [function(callback) {
-//     process.nextTick(function () {
-//       callback("a1", "a2");
-//     });
-//   }],
-//   B: ["A1", function (input, callback) {
-//     process.nextTick(function () {
-//       callback(input + "modified");
-//     });
-//   }],
-//   C: ["A0","B0", function (input1, input2, callback) {
-//     sys.p({
-//       input1: input1,
-//       input2: input2
-//     });
-//   }]
-// });
 
 Compose({
-  A: [fs.readFile, "data.txt"],
-  B: ["A1", sys.puts],
-  E: ["A0", sys.p]
-})()
+  A: [function(callback) {
+    process.nextTick(function () {
+      callback("a1", "a2");
+    });
+  }],
+  B: ["A1", function (input, callback) {
+    process.nextTick(function () {
+      callback(input + "modified");
+    });
+  }],
+  C: ["A0","B0", function (input1, input2, callback) {
+    sys.p({
+      input1: input1,
+      input2: input2
+    });
+  }]
+})();
+
+// Compose({
+//   A: [fs.readFile, "badFile.txt"],
+//   B: ["A1", sys.puts],
+//   E: ["A0", sys.p]
+// })()
 // 
 // Compose({
 //   A: [fs.readFile, __filename],
 //   B: ["A1", sys.puts],
 //   E: ["A0", sys.error]
-// })
-// 
+// })()
+
 // Compose({
 //   A: ["_0", fs.readFile],
 //   B: ["A1", sys.puts],
 //   E: ["A0", sys.error]
-// })('test')
+// })(__filename)
 // 
 // Compose({
 //   A: ["_0", fs.readFile],
